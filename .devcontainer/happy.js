59afc7976b2c5653ef71c436f0aebfc3df48b2ca
priyansh78626 <127733699+priyansh78626@users.noreply.github.com>
2025-01-31 18:08:53 +0000

    chnges

--- .devcontainer/happy.js
+++ .devcontainer/happy.js
@@ -1,1 +1,8 @@
 console.log("it is good to have learning daily")
+const accountId = 144567
+let accountEmail ="priyansh@gmail.com"
+var accountPassword = "123456"
+let accountCity = " Bhopal"
+//accountId = 2 not allowed//
+console.log(accountId);
+console.table([accountId, accountEmail, accountCity, accountPassword])
